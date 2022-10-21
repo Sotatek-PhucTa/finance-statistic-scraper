a8496d8647b992662dc5a3b9f949fc80903c4da7
hoangphuc <hoangphucnb97@gmail.com>
2022-10-21 14:10:23 +0700

    Add getBusiness rate vietcombank

--- bankinterestrate/vietcombank.go
+++ bankinterestrate/vietcombank.go
@@ -10,7 +10,7 @@ func init() {
 		},
 		RateInfo: BankRateInfo{
 			PersonalInterestRate: "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListLaiSuat.aspx?CusstomType=1&BacrhID=1&InrateType=&isEn=False&numAfter=2",
-			BusinessInterestRate: "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListLaiSuat.aspx?CusstomType=1&BacrhID=1&InrateType=&isEn=False&numAfter=2",
+			BusinessInterestRate: "https://portal.vietcombank.com.vn/UserControls/TVPortal.TyGia/pListLaiSuat.aspx?CusstomType=2&BacrhID=1&InrateType=&isEn=False&numAfter=2",
 		},
 	}
 	var vietcombankScraperHandler = GetVietcombankScraperHandler()
